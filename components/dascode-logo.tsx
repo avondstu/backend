7c9ea0c5b636266808cc3cce50eb7c20e1272633
Avond0410 <roynald.avond@gmail.com>
2025-01-08 15:27:42 +0700

    test images

--- components/dascode-logo.tsx
+++ components/dascode-logo.tsx
@@ -5,7 +5,7 @@ const ImageLogo = (props: IconProps) => {
     return (
         <>
             <Image
-            src='/images/logo/avond.png'
+            src={'/images/logo/avond.png'}
             width={100}
             height={100}
             alt='Logo Avond'
