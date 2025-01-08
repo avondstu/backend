17fa96c3169da2c9c2d2705fde1e1f9b3c9ed8b2
Avond0410 <roynald.avond@gmail.com>
2025-01-08 15:30:38 +0700

    link error

--- components/logo.tsx
+++ components/logo.tsx
@@ -15,16 +15,8 @@ const Logo = () => {
     const { hovered } = hoverConfig
     const isDesktop = useMediaQuery('(min-width: 1280px)');
 
-    if (config.sidebar === 'compact') {
-        return <Link href="/dashboard/analytics" className="flex gap-2 items-center   justify-center    ">
-            <ImageLogo className="  text-default-900 h-8 w-8 [&>path:nth-child(3)]:text-background [&>path:nth-child(2)]:text-background" />
-
-        </Link>
-    }
-    if (config.sidebar === 'two-column' || !isDesktop) return null
-
     return (
-        <Link href="/dashboard/analytics" className="flex gap-2 items-center    ">
+        <Link href="/" className="flex gap-2 items-center    ">
             <ImageLogo className="  text-default-900 h-8 w-8 [&>path:nth-child(3)]:text-background [&>path:nth-child(2)]:text-background" />
             {(!config?.collapsed || hovered) && (
                 <h1 className="text-xl font-semibold text-default-900 ">
