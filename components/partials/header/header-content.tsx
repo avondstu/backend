a87805fd2612adeb37e74e82160f21db692134ad
avondstu <roynald.avond@gmail.com>
2025-01-14 11:58:59 +0700

    test style 9

--- components/partials/header/header-content.tsx
+++ components/partials/header/header-content.tsx
@@ -8,10 +8,10 @@ const HeaderContent = ({ children }: { children: React.ReactNode }) => {
   const headerTheme = config.headerTheme !== 'light' && config.headerTheme !== 'transparent' ? ` dark theme-${config.headerTheme}` : `theme-${config.headerTheme}`
   if (config.sidebar === 'two-column') {
     return (
-      <header className={cn('top-0 z-50  m-6 rounded-lg default has-sticky-header', config.navbar, {
+      <header className={cn('flex-none    bg-header backdrop-blur-lg md:px-6 px-[15px] py-3    xl:ms-[248px]  flex items-center justify-between relative', config.navbar, {
         'has-sticky-header sticky top-6  px-6 ': config.navbar === 'floating'
       })}>
-        <div className={cn('flex-none   bg-header backdrop-blur-lg md:px-6 px-[15px] py-3    xl:ms-[300px] flex items-center justify-between  relative', headerTheme, {
+        <div className={cn('flex-none    bg-header backdrop-blur-lg md:px-6 px-[15px] py-3    xl:ms-[248px]  flex items-center justify-between relative', headerTheme, {
           'xl:ms-[72px]': config.subMenu || !config.hasSubMenu,
           'border-b': config.skin === 'bordered' && config.layout !== 'semi-box',
           'xl:ms-0': config.menuHidden || config.layout === "horizontal",
@@ -29,7 +29,7 @@ const HeaderContent = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <header className={cn('top-0 z-50 m-6 rounded-lg', config.navbar, {
+    <header className={cn('flex-none    bg-header backdrop-blur-lg md:px-6 px-[15px] py-3    xl:ms-[248px]  flex items-center justify-between relative', config.navbar, {
       'has-sticky-header sticky top-6  px-6 ': config.navbar === 'floating',
       'top-10 has-sticky-header': config.layout === 'compact',
       'has-sticky-header': config.layout === 'semi-box' && config.navbar !== 'floating',
--- components/partials/sidebar/sidebar-content.tsx
+++ components/partials/sidebar/sidebar-content.tsx
@@ -48,7 +48,7 @@ const SidebarContent = ({ children }: { children: React.ReactNode }) => {
             <div className=" relative  flex flex-col h-full  ">
                 {config.sidebarBgImage !== undefined && (
                     <div
-                        className=" absolute left-0 top-0   z-[-1] w-full h-full bg-cover bg-center opacity-[0.07]"
+                        className=" absolute left-0 top-0  z-[-1] w-full h-full bg-cover bg-center opacity-[0.07]"
                         style={{ backgroundImage: `url(${config.sidebarBgImage})` }}
                     ></div>
                 )}
